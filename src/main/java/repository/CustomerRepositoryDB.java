cbc7aa5f2f87774ad628f683bde220494c4f0564
Ankita <ankita.kumari@credable.in>
2022-02-20 21:12:40 +0530

    Banking-Demo

---
+++ src/main/java/repository/CustomerRepositoryDB.java
@@ -0,0 +1,34 @@
+package repository;
+import model.Customer;
+import java.sql.DriverManager;
+
+
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class CustomerRepositoryDB {
+    public List<Customer> findAll() {
+        List<Customer> customers = new ArrayList<>();
+        try {
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/banking?user=root&password=student");
+            PreparedStatement ps = conn.prepareStatement("SELECT customer_id,name,city from customers");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Customer customer = new Customer(
+                        rs.getString("customer_id"),
+                        rs.getString("name"),
+                        rs.getString("city"));
+
+                customers.add(customer);
+            }}
+        catch(SQLException e)
+            {
+                e.printStackTrace();
+            }
+            return customers;
+        }
+
+    }
---
+++ src/main/java/service/DefaultCustomerService.java
@@ -0,0 +1,23 @@
+package service;
+
+import model.Customer;
+import repository.CustomerRepositoryDB;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DefaultCustomerService {
+
+    private CustomerRepositoryDB repository = new CustomerRepositoryDB();
+
+    public List<Customer> getAllCustomers() {
+        return repository.findAll();
+    }
+    public List<Customer> getAllCustomersWithStars() {
+        repository.findAll().stream()
+                .map(c -> new Customer(c.getId(), c.getName(), c.getCity()))
+                .collect(Collectors.toList());
+        return repository.findAll();
+    }
+
+}
